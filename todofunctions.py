e77ce4e43a3e8797f3c25d995c9481ae7d9c4227
duvib231 <duvib231@gmail.com>
2024-04-28 09:45:51 +1000

    Minor changes to fix error during deployment

--- todofunctions.py
+++ todofunctions.py
@@ -1,13 +1,12 @@
-FILEPATH = "C:/Users/duvar/PycharmProjects/pythonProject/.venv/To-do list/todos.txt"
+default_filepath = "todos.txt"
 
-def read_todos(filepath = FILEPATH):
-    """Read file and make list. """
+
+def read_todos(filepath=default_filepath):
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath = FILEPATH):
-    """Write new todos into file"""
+def write_todos(todos_arg, filepath=default_filepath):
     with open(filepath, "w") as file_local:
         file_local.writelines(todos_arg)
